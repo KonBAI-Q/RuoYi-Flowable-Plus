863aa2fa33de2782a9938dcafb683aedb5048762
konbai <1527468660@qq.com>
2022-02-19 19:19:37 +0800

    !11sync -- 同步 RuoYi-Vue-Plus 更新 update 更新uuid去除默认值 漏改 fix 修复 insertOrUpdateBatch 获取模板错误 update 更新 所有 oss 均支持 https 配置 fix 修复分页组件请求两次问题(I4SQOR) update 去除编译器警告 规范写法
    update 更新uuid去除默认值 漏改
    fix 修复 insertOrUpdateBatch 获取模板错误
    update 更新 所有 oss 均支持 https 配置
    fix 修复分页组件请求两次问题(I4SQOR)
    update 去除编译器警告 规范写法
    add 增加页面更新说明
    fix 修复 minio 适配 https 导致的问题
    update 更新 多用户多设备的注释说明
    发布 4.0.0

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -56,7 +56,7 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2018-2021 konbai.work All Rights Reserved.</span>
+      <span>Copyright © 2021-2022 konbai.work All Rights Reserved.</span>
     </div>
   </div>
 </template>
