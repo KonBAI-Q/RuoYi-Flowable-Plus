cf81741a73f23064605b2ae54a52a0fc85fdd783
konbai <1527468660@qq.com>
2022-08-18 23:58:26 +0800

    style(前端流程发起): 简化代码逻辑

--- ruoyi-ui/src/views/workflow/work/start.vue
+++ ruoyi-ui/src/views/workflow/work/start.vue
@@ -72,21 +72,14 @@ export default {
       }
     },
     submit(data) {
-      if (data) {
-        const variables = data.valData;
-        // const formData = data.formData;
-        // formData.disabled = true;
-        // formData.formBtns = false;
-        if (this.definitionId) {
-          // formData.variables = variables;
-          // 启动流程并将表单数据加入流程变量
-          startProcess(this.definitionId, JSON.stringify(variables)).then(res => {
-            this.$modal.msgSuccess(res.msg);
-            this.$tab.closeOpenPage({
-              path: '/work/own'
-            })
+      if (data && this.definitionId) {
+        // 启动流程并将表单数据加入流程变量
+        startProcess(this.definitionId, JSON.stringify(data.valData)).then(res => {
+          this.$modal.msgSuccess(res.msg);
+          this.$tab.closeOpenPage({
+            path: '/work/own'
           })
-        }
+        })
       }
     }
   }
