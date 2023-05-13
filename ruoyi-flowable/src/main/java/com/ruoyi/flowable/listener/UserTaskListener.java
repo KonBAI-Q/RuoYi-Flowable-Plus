004575203cd445e0ecd853728b2bc103bb49a14a
konbai <1527468660@qq.com>
2023-05-13 19:08:24 +0800

    perf: 完善 用户任务监听器示例代码。

--- ruoyi-flowable/src/main/java/com/ruoyi/flowable/listener/UserTaskListener.java
+++ ruoyi-flowable/src/main/java/com/ruoyi/flowable/listener/UserTaskListener.java
@@ -2,16 +2,26 @@ package com.ruoyi.flowable.listener;
 
 import org.flowable.engine.delegate.TaskListener;
 import org.flowable.task.service.delegate.DelegateTask;
+import org.springframework.stereotype.Component;
 
 /**
- * @author Xuan xuan
- * @date 2021/4/20
+ * 用户任务监听器
+ *
+ * @author KonBAI
+ * @since 2023/5/13
  */
-public class UserTaskListener implements TaskListener{
+@Component(value = "userTaskListener")
+public class UserTaskListener implements TaskListener {
+
+    /**
+     * 注入字段（名称与流程设计时字段名称一致）
+     */
+    // private FixedValue field;
 
     @Override
     public void notify(DelegateTask delegateTask) {
-
+        //TODO 实现你的任务监听器逻辑
+        System.out.println("执行任务监听器...");
     }
 
 }
