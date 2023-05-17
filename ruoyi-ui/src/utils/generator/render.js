2e232de94289dd87194196d172ad5d891eaabf48
konbai <1527468660@qq.com>
2023-05-18 01:09:59 +0800

    refactor(系统工具): 重构 表单构建 的代码恢复与 RuoYi-Vue-Plus 代码一致

--- ruoyi-ui/src/utils/generator/render.js
+++ ruoyi-ui/src/utils/generator/render.js
@@ -1,121 +1,126 @@
-import { deepClone } from '@/utils/index'
+import { makeMap } from '@/utils/index'
 
-const componentChild = {}
-/**
- * 将./slots中的文件挂载到对象componentChild上
- * 文件名为key，对应JSON配置中的__config__.tag
- * 文件内容为value，解析JSON配置中的__slot__
- */
-const slotsFiles = require.context('./slots', false, /\.js$/)
-const keys = slotsFiles.keys() || []
-keys.forEach(key => {
-  const tag = key.replace(/^\.\/(.*)\.\w+$/, '$1')
-  componentChild[tag] = slotsFiles(key).default
-})
+// 参考https://github.com/vuejs/vue/blob/v2.6.10/src/platforms/web/server/util.js
+const isAttr = makeMap(
+  'accept,accept-charset,accesskey,action,align,alt,async,autocomplete,'
+  + 'autofocus,autoplay,autosave,bgcolor,border,buffered,challenge,charset,'
+  + 'checked,cite,class,code,codebase,color,cols,colspan,content,http-equiv,'
+  + 'name,contenteditable,contextmenu,controls,coords,data,datetime,default,'
+  + 'defer,dir,dirname,disabled,download,draggable,dropzone,enctype,method,for,'
+  + 'form,formaction,headers,height,hidden,high,href,hreflang,http-equiv,'
+  + 'icon,id,ismap,itemprop,keytype,kind,label,lang,language,list,loop,low,'
+  + 'manifest,max,maxlength,media,method,GET,POST,min,multiple,email,file,'
+  + 'muted,name,novalidate,open,optimum,pattern,ping,placeholder,poster,'
+  + 'preload,radiogroup,readonly,rel,required,reversed,rows,rowspan,sandbox,'
+  + 'scope,scoped,seamless,selected,shape,size,type,text,password,sizes,span,'
+  + 'spellcheck,src,srcdoc,srclang,srcset,start,step,style,summary,tabindex,'
+  + 'target,title,type,usemap,value,width,wrap'
+)
 
-function vModel(dataObject, defaultValue) {
+function vModel(self, dataObject, defaultValue) {
   dataObject.props.value = defaultValue
 
   dataObject.on.input = val => {
-    this.$emit('input', val)
+    self.$emit('input', val)
   }
 }
 
-function mountSlotFiles(h, confClone, children) {
-  const childObjs = componentChild[confClone.__config__.tag]
-  if (childObjs) {
-    Object.keys(childObjs).forEach(key => {
-      const childFunc = childObjs[key]
-      if (confClone.__slot__ && confClone.__slot__[key]) {
-        children.push(childFunc(h, confClone, key))
-      }
-    })
-  }
-}
-
-function emitEvents(confClone) {
-  ['on', 'nativeOn'].forEach(attr => {
-    const eventKeyList = Object.keys(confClone[attr] || {})
-    eventKeyList.forEach(key => {
-      const val = confClone[attr][key]
-      if (typeof val === 'string') {
-        confClone[attr][key] = event => this.$emit(val, event)
-      }
-    })
-  })
-}
-
-function buildDataObject(confClone, dataObject) {
-  Object.keys(confClone).forEach(key => {
-    const val = confClone[key]
-    if (key === '__vModel__') {
-      vModel.call(this, dataObject, confClone.__config__.defaultValue)
-    } else if (dataObject[key] !== undefined) {
-      if (dataObject[key] === null
-        || dataObject[key] instanceof RegExp
-        || ['boolean', 'string', 'number', 'function'].includes(typeof dataObject[key])) {
-        dataObject[key] = val
-      } else if (Array.isArray(dataObject[key])) {
-        dataObject[key] = [...dataObject[key], ...val]
-      } else {
-        dataObject[key] = { ...dataObject[key], ...val }
-      }
-    } else {
-      dataObject.attrs[key] = val
+const componentChild = {
+  'el-button': {
+    default(h, conf, key) {
+      return conf[key]
+    },
+  },
+  'el-input': {
+    prepend(h, conf, key) {
+      return <template slot="prepend">{conf[key]}</template>
+    },
+    append(h, conf, key) {
+      return <template slot="append">{conf[key]}</template>
     }
-  })
-
-  // 清理属性
-  clearAttrs(dataObject)
-}
-
-function clearAttrs(dataObject) {
-  delete dataObject.attrs.__config__
-  delete dataObject.attrs.__slot__
-  delete dataObject.attrs.__methods__
-}
-
-function makeDataObject() {
-  // 深入数据对象：
-  // https://cn.vuejs.org/v2/guide/render-function.html#%E6%B7%B1%E5%85%A5%E6%95%B0%E6%8D%AE%E5%AF%B9%E8%B1%A1
-  return {
-    class: {},
-    attrs: {},
-    props: {},
-    domProps: {},
-    nativeOn: {},
-    on: {},
-    style: {},
-    directives: [],
-    scopedSlots: {},
-    slot: null,
-    key: null,
-    ref: null,
-    refInFor: true
+  },
+  'el-select': {
+    options(h, conf, key) {
+      const list = []
+      conf.options.forEach(item => {
+        list.push(<el-option label={item.label} value={item.value} disabled={item.disabled}></el-option>)
+      })
+      return list
+    }
+  },
+  'el-radio-group': {
+    options(h, conf, key) {
+      const list = []
+      conf.options.forEach(item => {
+        if (conf.optionType === 'button') list.push(<el-radio-button label={item.value}>{item.label}</el-radio-button>)
+        else list.push(<el-radio label={item.value} border={conf.border}>{item.label}</el-radio>)
+      })
+      return list
+    }
+  },
+  'el-checkbox-group': {
+    options(h, conf, key) {
+      const list = []
+      conf.options.forEach(item => {
+        if (conf.optionType === 'button') {
+          list.push(<el-checkbox-button label={item.value}>{item.label}</el-checkbox-button>)
+        } else {
+          list.push(<el-checkbox label={item.value} border={conf.border}>{item.label}</el-checkbox>)
+        }
+      })
+      return list
+    }
+  },
+  'el-upload': {
+    'list-type': (h, conf, key) => {
+      const list = []
+      if (conf['list-type'] === 'picture-card') {
+        list.push(<i class="el-icon-plus"></i>)
+      } else {
+        list.push(<el-button size="small" type="primary" icon="el-icon-upload">{conf.buttonText}</el-button>)
+      }
+      if (conf.showTip) {
+        list.push(<div slot="tip" class="el-upload__tip">只能上传不超过 {conf.fileSize}{conf.sizeUnit} 的{conf.accept}文件</div>)
+      }
+      return list
+    }
   }
 }
 
 export default {
-  props: {
-    conf: {
-      type: Object,
-      required: true
-    }
-  },
   render(h) {
-    const dataObject = makeDataObject()
-    const confClone = deepClone(this.conf)
-    const children = this.$slots.default || []
+    const dataObject = {
+      attrs: {},
+      props: {},
+      on: {},
+      style: {}
+    }
+    const confClone = JSON.parse(JSON.stringify(this.conf))
+    const children = []
 
-    // 如果slots文件夹存在与当前tag同名的文件，则执行文件中的代码
-    mountSlotFiles.call(this, h, confClone, children)
+    const childObjs = componentChild[confClone.tag]
+    if (childObjs) {
+      Object.keys(childObjs).forEach(key => {
+        const childFunc = childObjs[key]
+        if (confClone[key]) {
+          children.push(childFunc(h, confClone, key))
+        }
+      })
+    }
 
-    // 将字符串类型的事件，发送为消息
-    emitEvents.call(this, confClone)
-
-    // 将json表单配置转化为vue render可以识别的 “数据对象（dataObject）”
-    buildDataObject.call(this, confClone, dataObject)
-
-    return h(this.conf.__config__.tag, dataObject, children)
-  }
+    Object.keys(confClone).forEach(key => {
+      const val = confClone[key]
+      if (key === 'vModel') {
+        vModel(this, dataObject, confClone.defaultValue)
+      } else if (dataObject[key]) {
+        dataObject[key] = val
+      } else if (!isAttr(key)) {
+        dataObject.props[key] = val
+      } else {
+        dataObject.attrs[key] = val
+      }
+    })
+    return h(this.conf.tag, dataObject, children)
+  },
+  props: ['conf']
 }
